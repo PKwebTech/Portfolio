e7cf3a31ec38b140e73e91d3c51b5557ab1259a7
Pravin kolate <88478158+PKwebTech@users.noreply.github.com>
2022-09-02 12:45:25 +0530

    Updates

---
+++ precache-manifest.93f30bfe8440eb0945edc790e591525d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc295e827acb4779e2a3c132faee159",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0778d1cea6f160c5d62a",
+    "url": "/Portfolio/static/css/main.877853c5.chunk.css"
+  },
+  {
+    "revision": "de1b85c937e8525816af",
+    "url": "/Portfolio/static/js/2.caecfd65.chunk.js"
+  },
+  {
+    "revision": "b114cc85da504a772f040e3f40f8e46a",
+    "url": "/Portfolio/static/js/2.caecfd65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0778d1cea6f160c5d62a",
+    "url": "/Portfolio/static/js/main.ae4dd179.chunk.js"
+  },
+  {
+    "revision": "efed155d89bc85b81984",
+    "url": "/Portfolio/static/js/runtime-main.88fabfc4.js"
+  },
+  {
+    "revision": "1559ff00329f6ae4f7c6ae3284d09ce0",
+    "url": "/Portfolio/static/media/back.1559ff00.jpg"
+  }
+]);
