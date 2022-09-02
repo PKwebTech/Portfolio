6501a00de12021f4282027f0a589eaf80dba7990
Pravin kolate <88478158+PKwebTech@users.noreply.github.com>
2022-09-02 13:27:52 +0530

    Create a React app and publish it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import './index.css'
-import {BrowserRouter as Router} from 'react-router-dom'
-
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
+  <App/>
+  ,document.getElementById('root')
+)
